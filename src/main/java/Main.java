808abda5c0c3ce8435ca71420f46df7ce82e2f7c
MarcCasadevall <apple5.92@hotmail.com>
2026-02-09 12:45:07 +0100

    corrections,final,static, and names

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         InPersonWorker person = new InPersonWorker("Pablo ", "Lopez", 10,80);
-        OnlineWorker online = new OnlineWorker("Pedro ","Gonazlez", 12, 30);
+        OnlineWorker online = new OnlineWorker("Pedro ","Gonzalez", 12, 30);
 
         System.out.println(person.calculateSalary(40));
         System.out.println(online.calculateSalary(40));
